e8c3c3335304dec676036c17a46857b5722d1f94
Joey Andres <joey.andres@imperva.com>
2022-07-09 18:19:38 -0700

    Forgot import process.

--- src/lib/hr_sleep.ts
+++ src/lib/hr_sleep.ts
@@ -1,8 +1,11 @@
+import { hrtime } from 'process';
+
 export function hrSleep(microsecs: number) {
-  const hrTime = process.hrtime();
+  let hrTime = hrtime();
   const startTimeMicroSec = hrTime[0] * 1000000 + hrTime[1] / 1000;
   let delta = 0;
   while (delta >= microsecs) {
+    hrTime = hrtime();
     const endTimeMicroSec = hrTime[0] * 1000000 + hrTime[1] / 1000;
     delta = endTimeMicroSec - startTimeMicroSec;
   }
