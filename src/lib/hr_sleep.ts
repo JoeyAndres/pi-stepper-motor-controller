070d43f3192985218cf6aaa9dfba4ee6c3208121
Joey Andres <joey.andres@imperva.com>
2022-07-09 18:55:37 -0700

    Fix hr_sleep.ts.

--- src/lib/hr_sleep.ts
+++ src/lib/hr_sleep.ts
@@ -1,12 +1,12 @@
 import { hrtime } from 'process';
 
 export function hrSleep(microsecs: number) {
-  let hrTime = hrtime();
-  const startTimeMicroSec = hrTime[0] * 1000000 + hrTime[1] / 1000;
+  let hrTime = hrtime.bigint();
+  const startTimeMicroSec = hrTime[0] * 1000000 + hrTime[1] * 1000;
   let delta = 0;
   while (delta >= microsecs) {
-    hrTime = hrtime();
-    const endTimeMicroSec = hrTime[0] * 1000000 + hrTime[1] / 1000;
+    hrTime = hrtime.bigint();
+    const endTimeMicroSec = hrTime[0] * 1000000 + hrTime[1] * 1000;
     delta = endTimeMicroSec - startTimeMicroSec;
   }
 }
